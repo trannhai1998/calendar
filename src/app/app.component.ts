f33627f9044d0dc324bfde764081970fdc6770f2
trannamhai98 <haitn@tekup.vn>
2021-03-09 15:34:03 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    height: "auto",
+    dayCellClassNames: "calendar-daycell",
+    aspectRatio: 1.2,
+    viewClassNames: "calendar",
+    buttonText: {
+      today: ""
+    },
+    eventMinHeight: 0,
+    weekends: true,
+    events: {
+      backgroundColor: "#ffd24a",
+      
+      events: [
+        { title: 'Event 1', date: '2021-03-11' },
+        { title: 'Event 2', date: '2021-03-13' }
+      ]
+    }
+    
+  };
   title = 'angular-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+]);
 
 @NgModule({
   declarations: [
@@ -11,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
